495c0b41f9ba78c2e1bcf0b64330673037d36743
Kleowulf <claytond921@gmail.com>
2024-09-09 09:23:19 -0600

    added all win conditions

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ let grid = {
     upperMiddleImg: document.getElementById('upperMiddleImg').src,
     upperRightImg: document.getElementById('upperRightImg').src,
     middleLeftImg: document.getElementById('middleLeftImg').src,
+    middleMiddleImg: document.getElementById('middleMiddleImg').src,
+    middleRightImg: document.getElementById('middleRightImg').src,
+    lowerLeftImg: document.getElementById('lowerLeftImg').src,
+    lowerMiddleImg: document.getElementById('lowerMiddleImg').src,
+    lowerRightImg: document.getElementById('lowerRightImg').src,
 }
 
 function updateGrid () {
@@ -10,6 +15,24 @@ function updateGrid () {
     grid.upperMiddleImg = document.getElementById('upperMiddleImg').src;
     grid.upperRightImg = document.getElementById('upperRightImg').src;
     grid.middleLeftImg = document.getElementById('middleLeftImg').src;
+    grid.middleMiddleImg = document.getElementById('middleMiddleImg').src;
+    grid.middleRightImg = document.getElementById('middleMiddleImg').src;
+    grid.lowerLeftImg = document.getElementById('lowerLeftImg').src;
+    grid.lowerMiddleImg = document.getElementById('lowerMiddleImg').src;
+    grid.lowerRightImg = document.getElementById('lowerRightImg').src;
+}
+
+function checkWinConditions(trainerImage) {
+    return {
+        topRow: grid.upperLeftImg === trainerImage && grid.upperMiddleImg === trainerImage && grid.upperRightImg === trainerImage,
+        middleRow: grid.middleLeftImg === trainerImage && grid.middleMiddleImg === trainerImage && grid.middleRightImg === trainerImage,
+        bottomRow: grid.lowerLeftImg === trainerImage && grid.lowerMiddleImg === trainerImage && grid.lowerRightImg === trainerImage,
+        leftColumn: grid.upperLeftImg === trainerImage && grid.middleLeftImg === trainerImage && grid.lowerLeftImg === trainerImage,
+        middleColumn: grid.upperMiddleImg === trainerImage && grid.middleMiddleImg === trainerImage && grid.lowerMiddleImg === trainerImage,
+        rightColumn: grid.upperRightImg === trainerImage && grid.middleRightImg === trainerImage && grid.lowerRightImg === trainerImage,
+        leftCross: grid.upperLeftImg === trainerImage && grid.middleMiddleImg === trainerImage && grid.lowerRightImg === trainerImage,
+        rightCross: grid.upperRightImg === trainerImage && grid.middleMiddleImg ===trainerImage && grid.lowerLeftImg === trainerImage,
+    }
 }
 
 
@@ -28,7 +51,9 @@ class Trainer {
     placement () {
         this.location.src = this.image;
         updateGrid();
-        if(grid.upperLeftImg === this.image && grid.upperMiddleImg ===  this.image && grid.upperRightImg === this.image){
+        const winConditions = checkWinConditions(this.image);
+
+        if(winConditions.topRow || winConditions.middleRow || winConditions.bottomRow || winConditions.leftColumn || winConditions.middleColumn || winConditions.rightColumn || winConditions.rightCross || winConditions.leftCross){
             document.querySelector('h2').innerText = `${this._name} wins`
         }
 
